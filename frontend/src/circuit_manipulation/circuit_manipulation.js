b6b749b30016d329a48d0666d1e38a0d6ade406a
bruno <bweisz@eryxsoluciones.com.ar>
2024-11-16 15:06:24 -0300

    Adds circuit manipulation in the frontend

---
+++ frontend/src/circuit_manipulation/circuit_manipulation.js
@@ -0,0 +1,60 @@
+import { compile, createFileManager } from '@noir-lang/noir_wasm';
+import fs
+
+export default class CircuitBuilder {
+
+    function defaultNargoToml(){
+        return `[package]
+              name = "poapPossessionProver"
+              type = "bin"
+            
+              [dependencies]
+              `
+    }
+
+    function stringToStream(text: string): ReadableStream<Uint8Array> {
+        const encoder = new TextEncoder();
+        const uint8array = encoder.encode(text);
+        return new ReadableStream({
+            start(controller) {
+                controller.enqueue(uint8array);
+                controller.close();
+            }
+        });
+    }
+
+    function read_circuit_from_file(){
+        let file_circuit_path = "../../loquesea";
+        const circuit_src = = fs.readFileSync(filePath, 'utf-8');
+    }
+
+    async function compileCircuit() {
+        const fm = createFileManager('/');
+        await fm.writeFile('./src/main.nr', this.stringToStream(this.read_circuit_from_file()));
+        await fm.writeFile('./Nargo.toml', this.stringToStream(this.defaultNargoToml()));
+        try {
+            const result = await compile(fm);
+            return result.program;
+        } catch(e){
+            throw new Error("Compilation failed: " + e)
+        }
+    }
+
+    async function generateProof(inputs){
+        let currentCompiledCircuit = this.compileCircuit()
+        const noir = new Noir(currentCompiledCircuit);
+        console.log("Initializing Noir")
+        await noir.init();
+        console.log("Noir Initialized")
+        const { witness } = await noir.execute(inputs);
+        console.log("Executed circuit")
+        if (!witness) return;
+        const barretenbergBackend = new BarretenbergBackend(currentCompiledCircuit, { threads: navigator.hardwareConcurrency });
+        console.log("Barretenberg initialized")
+        const proofData = await barretenbergBackend.generateProof(witness)
+        console.log("Proof generated")
+        if (!proofData) return;
+        
+    };
+
+}
