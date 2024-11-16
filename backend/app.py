c7dbfd3a8b533a0081d1de3f60e2232d18f7bf56
Yago Pajariño <yagopajarino@outlook.com>
2024-11-17 03:17:40 +0700

    Agrega cors

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
 from poap_handlers import PoapHandlers
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=["GET"])
