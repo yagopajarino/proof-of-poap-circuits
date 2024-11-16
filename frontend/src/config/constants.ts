fe4471e57359566db1645f107624a1fdc30614a5
NicolasBiondini <nicolasbiondini29@gmail.com>
2024-11-17 04:35:46 +0700

    functionality: fetch smart contracts

---
+++ frontend/src/config/constants.ts
@@ -0,0 +1,4 @@
+const BACKEND_URL = "https://proof-of-poap-circuits.onrender.com/api/";
+const CONTRACT_ADDRESS = "0x3b82afC74BD691b2c7fec7A98e57F69D119d59B9";
+
+export { BACKEND_URL, CONTRACT_ADDRESS };
